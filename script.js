c7671bdf92755d8217df5b520139f26e651c27ce
ujjwal05062002 <ujjwal05062002@gmail.com>
2026-05-18 17:38:38 +0530

    vowel consonants count

---
+++ script.js
@@ -0,0 +1,10 @@
+// wap to calculate the lowercase and uppercase vowel and consonants in a string 
+
+
+let str = prompt("Enter your string please : ")
+
+let lowerVowel = 0
+let lowerConsonants = 0
+let upperVowel = 0
+let upperConsonants = 0
+
