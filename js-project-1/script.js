9eea6d4b0898a2585186dfcf72437eb8eb2d0ce6
ujjwal05062002 <ujjwal05062002@gmail.com>
2026-06-07 00:58:09 +0530

    variables declaration added

---
+++ js-project-1/script.js
@@ -0,0 +1,17 @@
+// VARIABLES 
+
+const accountId = 2345  // number
+let accountEmail = "ujjwal@gmail.com" // string  let => can be changed
+var accountPassword = "12345"  // string
+let accountState     // undefined
+
+// accountId = 23111   --> const can't be changed once declared
+
+
+console.log(accountId);
+
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
--- script.js
+++ script.js
@@ -109,3 +109,5 @@ for (let i=0; i<arr.length; i++){
         console.log(arr[i] +  " : "  + count);
     }
 }
+
+
